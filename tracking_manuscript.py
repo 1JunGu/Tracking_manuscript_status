8144e81da2cb1633b2f97834478f8fcee8932aea
Jun Gu <kyuinkoo@mail.ustc.edu.cn>
2024-05-24 01:05:36 +0800

    On branch jungu
      Add another manuscript system
    
     Changes to be committed:
    	modified:   tracking_manuscript.py

--- tracking_manuscript.py
+++ tracking_manuscript.py
@@ -15,11 +15,11 @@ from selenium.common.exceptions import TimeoutException
 from bs4 import BeautifulSoup
 
 class TrackingManuscript:
-    def __init__(self, username, password, logger):
+    def __init__(self, username, password, logger, site_type):
         self.logger = logger  # logger object
         self.username = username  # manuscript username
         self.password = password  # manuscript password
-        self.previous_stage_info = "None"  # previous stage information
+        self.site_type = site_type #plex or scholarOne
 
     def handle_popup(self, driver, timeout=5):
         button_text = ["Continue","Accept"] #only for the two buttons
@@ -91,52 +91,73 @@ class TrackingManuscript:
             self.logger.critical(f"fatal error: using all strategies failed to find the '{link_text}' link.")
             return None
 
-    def extract_current_stage(self,driver, text_stage="Current Stage"):
+    def extract_current_stage(self,driver): #use this function for plex
         # Use BeautifulSoup to parse the page HTML
         soup = BeautifulSoup(driver.page_source, "html.parser")
-
         # Find the <th> element containing the text_stage
-        current_stage_header = soup.find("th", string=lambda text: text_stage in text if text else False)
-
-        if current_stage_header:
-            # Extract the actual stage information in the next <td> element
-            current_stage_cell = current_stage_header.find_next_sibling("td")
-            if current_stage_cell:
-                current_stage = current_stage_cell.text.strip()
+        if self.site_type == "plex":
+            text_stage = "Current Stage"
+            # Find the <th> element containing the text_stage
+            current_stage_header = soup.find("th", string=lambda text: text_stage in text if text else False)
+            if current_stage_header:
+                # Extract the actual stage information in the next <td> element
+                current_stage = current_stage_header.find_next_sibling("td").text.strip()
                 return current_stage
             else:
-                self.logger.error(f"Header found, but No {text_stage} information found.")
+                self.logger.error(f"No header and {text_stage} information found.")
                 return None
-        else:
-            self.logger.error(f"No header and {text_stage} information found.")
-            return None
+        elif self.site_type == "scholarOne":
+            # Find the <td> element with the specified style and class
+            status_cell = soup.find("td", style="border-bottom:none",text=True) #By adding text=True, we ensure that the <td> element contains text content 
+            if status_cell:
+                current_stage = status_cell.find("span", class_="pagecontents").get_text(strip=True)
+                return current_stage
+            else:
+                self.logger.error(f"no status cell found.")
+                return None
+
 
     def extract_stage_table(self,driver):
         # Use BeautifulSoup to parse the page HTML
         soup = BeautifulSoup(driver.page_source, "html.parser")
 
-        # Find the table with the desired structure
-        table = soup.find("table", attrs={"border": "5"}) #border = 5
+        if self.site_type == "plex":
+            # Find the table with the desired structure
+            table = soup.find("table", attrs={"border": "5"}) #border = 5
 
-        if table:
-            # Check if the table has the desired header cells
-            header_cells = table.find_all("th")
-            if len(header_cells) >= 2 and header_cells[0].get_text(strip=True) == "Stage" and header_cells[1].get_text(strip=True) == "Start Date":
+            if table:
+                # Check if the table has the desired header cells
+                header_cells = table.find_all("th")
+                if len(header_cells) >= 2 and header_cells[0].get_text(strip=True) == "Stage" and header_cells[1].get_text(strip=True) == "Start Date":
+                    # Extract the table HTML
+                    table_html = str(table)
+                    self.logger.debug("Extracted the stage table.")
+                    return table_html
+                else:
+                    self.logger.error("Table does not have the desired header cells.")
+                    raise ValueError("Table does not have the desired header cells.")
+            else:
+                self.logger.error("Table not found.")
+                raise ValueError("Table not found.")
+
+        elif self.site_type == "scholarOne":
+            # Find the table element by its id
+            table = soup.find("table", attrs={"id": "authorDashboardQueue"})
+            if table:
                 # Extract the table HTML
                 table_html = str(table)
                 self.logger.debug("Extracted the stage table.")
-                return table_html
+                return table_html       
             else:
-                self.logger.error("Table does not have the desired header cells.")
-                return None
-        else:
-            self.logger.error("Table not found.")
-            return None
+                self.logger.error("Table not found.")
+                raise ValueError("Table not found.")
+            
 
-    def activate_browser(self):
+    def activate_browser(self,headless=True):
         options = EdgeOptions()
         options.use_chromium = True # use the chromium version of Edge
-        options.add_argument('--headless') # run the browser in headless mode
+        if headless:
+            options.add_argument('--headless')
         options.add_experimental_option('detach',True) # detach the browser
         # activate the browser
         driver = webdriver.Edge(options=options)
@@ -146,41 +167,49 @@ class TrackingManuscript:
         # navigate to the website
         driver.get(login_url)
         self.logger.debug("Navigated to the website")
-
         # wait for 10 seconds
-        driver.implicitly_wait(10)
-
-        self.handle_popup(driver, timeout=10)
-
-        # Note: the below code requires the Selenium version 4.0 or higher
-        driver.find_element(by=By.ID, value="login").send_keys(self.username)
-        driver.find_element(by=By.ID, value="password").send_keys(self.password)
-        driver.find_element(By.XPATH, "//input[@value='Login']").click()
-        self.logger.debug("Filled the login form and clicked the 'Login' button")
-        self.handle_popup(driver,timeout=5)
-
-        # Check if the "Live Manuscripts" link exists and navigate to it
-        self.find_and_click_link(driver, "Live Manuscripts", timeout=5)
-        self.handle_popup(driver, timeout=5)
-        # Check if the "Check Status" link exists and navigate to it
-        self.find_and_click_link(driver, "Check Status", timeout=5)
+        driver.implicitly_wait(5)
         self.handle_popup(driver, timeout=5)
 
-        return driver
+        if self.site_type == "plex":
+            # Note: the below code requires the Selenium version 4.0 or higher
+            driver.find_element(by=By.ID, value="login").send_keys(self.username)
+            driver.find_element(by=By.ID, value="password").send_keys(self.password)
+            driver.find_element(By.XPATH, "//input[@value='Login']").click()
+            self.logger.debug("Filled the login form and clicked the 'Login' button")
+            self.handle_popup(driver,timeout=2)
 
-    def check_status(self,driver,method,previous_stage_info):
+            # Check if the "Live Manuscripts" link exists and navigate to it
+            self.find_and_click_link(driver, "Live Manuscripts", timeout=5)
+            self.handle_popup(driver, timeout=2)
+            # Check if the "Check Status" link exists and navigate to it
+            self.find_and_click_link(driver, "Check Status", timeout=5)
+            self.handle_popup(driver, timeout=2)
+            return driver
+        elif self.site_type == "scholarOne":
+            # Note: the below code requires the Selenium version 4.0 or higher
+            driver.find_element(by=By.ID, value="USERID").send_keys(self.username)
+            driver.find_element(by=By.ID, value="PASSWORD").send_keys(self.password)
+            driver.find_element(by=By.ID, value="logInButton").click()
+            self.logger.debug("Filled the login form and clicked the 'Login' button")
+
+            driver.find_element(By.XPATH, "//a[contains(@href, 'AUTHOR_VIEW_MANUSCRIPTS')]").click()
+            self.logger.debug("Navigate to the author page")
+
+            return driver
+
+    def monitor_status_changes(self, driver,notification_method, previous_stage_info):
         driver.refresh()
         self.logger.debug("Refreshed the page") #the time interval should be less than 30 minutes
-        self.handle_popup(driver, timeout=5)
-        # Extract the "Current Stage" information
-        current_stage_info = self.extract_current_stage(driver,text_stage="Current Stage")
+        current_table = self.extract_stage_table(driver)
+        current_status = self.extract_current_stage(driver)
 
-        if current_stage_info  != previous_stage_info:
+        if current_status != previous_stage_info:
             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            message = f"Current Stage: {current_stage_info}, Now : {current_time}"
+            message = f"Current Stage: {current_status}, Now : {current_time}"
             self.logger.status(message)
-            if method == "wechat":
+            previous_stage_info = current_status
+            if notification_method == "wechat":
                 return message
-            elif method == "email":
-                stage_table = self.extract_stage_table(driver)
-                return message, stage_table, current_stage_info
+            elif notification_method == "email":
+                return message, current_table, current_status
