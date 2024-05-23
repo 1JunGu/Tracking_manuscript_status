145889aa94a34c9114d77b690707cf5926341428
Jun Gu <kyuinkoo@mail.ustc.edu.cn>
2024-05-24 01:13:59 +0800

    update template

--- tracking_journal_template.py
+++ tracking_journal_template.py
@@ -21,22 +21,22 @@ smtp_password = "xxxxxxxxxxxxxxxx"  # Authorization code
 receiver_email = "xxxxxxxxxxxxxx"
 
 logger = MyLog.MyLogger(name="log")
-tm_nc = tm.TrackingManuscript(username,password,logger)
+tm_my = tm.TrackingManuscript(username,password,logger,"plex") or "scholarOn"
+
+method = "email" # or wechat
 
 """
 Main code
 """
 #activate the browser
-driver = tm_nc.activate_browser()
-
+driver = tm_my.activate_browser(headless=True)
 # navigate to the website
-driver = tm_nc.login_and_navigate(driver,login_url)
+driver = tm_my.login_and_navigate(driver,login_url)
 
 previous_stage_info = "None"
-method = "email" # or wechat
 while True:
     try:
-        status_message, status_table = tm_nc.check_status(driver,"email")
+        status_message, status_table, previous_stage_info = tm_my.monitor_status_changes(driver,"email",previous_stage_info)
         if method == "wechat":  
             wechat.sendMsg(openId,status_message)
         elif method == "email":
@@ -46,6 +46,13 @@ while True:
         logger.info("Waiting for 30 minutes")
         time.sleep(1800)
     except Exception as e:
-        logger.error(f"An error occurred: {str(e)}")
-        logger.info("Waiting for 5 minutes before retrying")
-        time.sleep(300)
+        logger.critical(f"An error occurred: {str(e)}")
+        wechat.sendMsg(openId,"An error occurred, please check the log")
+        logger.info("retrying")
+        driver = tm_my.login_and_navigate(driver,login_url)
+    except ValueError("unknown site type"):
+        logger.critical("unknown site type")
+        break
+
+driver.quit()
+
